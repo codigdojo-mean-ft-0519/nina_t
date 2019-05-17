4e4b96b4278dfa387eb74c8952c8e5385c9f2c93
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-05-16 20:33:24 -0700

    adding project folder for restful tasks

---
+++ 05-angular/02-fetch-data/restful_tasks/server/controllers/task.controller.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Task = mongoose.model("Task");
+
+module.exports = {
+  //GET: Retrieve all Tasks
+  index(request, response) {
+    Task.find(request.body)
+      .then(task => response.json(tasks))
+      .catch(error => response.json(error));
+  },
+  //GET: Retrieve a Task by ID
+  show(request, response) {
+    Task.findById(request.params.id)
+      .then(task => response.json(task))
+      .catch(error => response.json(error));
+  },
+  //POST: Create a Task
+  create(request, response) {
+    Task.create(request.body)
+      .then(task => response.json(task))
+      .catch(error => response.json(error));
+  },
+  //PUT: Update a Task by ID
+  update(request, response) {
+    Task.findByIdAndUpdate(request.params.id, request.body, {
+        new: true
+      })
+      .then(task => response.json(task))
+      .catch(error => response.json(task))
+  },
+  //DELETE: Delete a Task by ID
+  destroy(request, response) {
+    Task.findByIdAndDelete(request.params.id)
+      .then(result => response.json(result))
+      .catch(error => response.json(error));
+  },
+};
