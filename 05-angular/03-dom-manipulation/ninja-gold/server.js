576be653970120bdcbf98d05257dde06a249e637
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-05-18 21:32:15 -0700

    starting ninja gold

---
+++ 05-angular/03-dom-manipulation/ninja-gold/server.js
@@ -0,0 +1,20 @@
+const parser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+const {
+  env: {
+    PORT: port = 8000
+  },
+} = process;
+const app = express();
+
+app.use(parser.urlencoded({
+  extended: true
+}));
+app.use(parser.json());
+app.use(express.static(path.join(__dirname, 'dist/ninja-gold')));
+
+console.log(path.join(__dirname, 'dist/books'));
+
+app.listen(port, () => console.log(`express server listening on port ${port}`));
