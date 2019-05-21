7cc75816b57d6e03a084a323a8f5b5c928016ca9
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-05-21 12:15:47 -0700

    finished css/html framework for crud

---
+++ 05-angular/03-dom-manipulation/restful-tasks-crud/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { Task } from './task.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  //DEFINE AND ASSIGN INSTANCE VARIABLES ABOVE THE CONSTRUCTOR
+  title = 'Restful Tasks CRUD';
+  tasks: Task[];
+  task: Task;
+
+  constructor(private _httpService: HttpService) {}
+  //This allows us to see things in our html later
+
+  ngOnInit() {
+    //REPLACE THIS STRING WITH A REAL ID LATER
+    const id = '5cdcd455643b25023c99080e';
+    this.getTasks();
+    //I have no idea how to represent an id from our model...Using 1 as a placeholder
+    this.getTask(id);
+  }
+  getTasks() {
+    const observable: Observable<Task[]> = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log('Got our tasks!', data);
+      this.tasks = data;
+    });
+  }
+  getTask(id: string) {
+    console.log('From Get Task, our id is ' + id);
+    this._httpService.getTask(id).subscribe(task => {
+      //Task will show us [Object object]...but if you put a string ahead of it it won't be
+      console.log('Got a task!', task);
+      this.task = task;
+    });
+  }
+}
---
+++ 05-angular/03-dom-manipulation/restful-tasks-crud/src/app/http.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from './task.model';
+import { Observable } from 'rxjs';
+
+//have a majority of your interaction with the resource (i.e. tasks) in the same service file
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private _http: HttpClient) {
+    this.getTasks();
+  }
+
+  getTasks(): Observable<Task[]> {
+    return this._http.get<Task[]>('/tasks');
+  }
+  //Observable says "I'm going to wrap up the result of this in something that's ansynchronous"
+  getTask(id: string): Observable<Task> {
+    console.log('From our HTTP Service, our id is ' + id);
+    return this._http.get<Task>(`/tasks/${id}`);
+  }
+}
