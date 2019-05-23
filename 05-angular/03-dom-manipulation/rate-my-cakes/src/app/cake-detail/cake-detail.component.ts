c9adb51a9d85dca6fce900a704f91be56f1c6407
Nina Tompkin <ninatompkin@Ninas-MacBook-Pro.local>
2019-05-23 14:28:23 -0700

    finished rate-my-cakes with average

--- 05-angular/03-dom-manipulation/rate-my-cakes/src/app/cake-detail/cake-detail.component.ts
+++ 05-angular/03-dom-manipulation/rate-my-cakes/src/app/cake-detail/cake-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Cake, Rating } from '../cake.model';
 
 @Component({
@@ -6,18 +6,31 @@ import { Cake, Rating } from '../cake.model';
   templateUrl: './cake-detail.component.html',
   styleUrls: ['./cake-detail.component.css'],
 })
-export class CakeDetailComponent implements OnInit {
+export class CakeDetailComponent implements OnInit, OnChanges {
+  avg: number | string = 0;
   @Input() cakeToShow: Cake;
 
   constructor() {}
 
-  getAvg(arr) {
-    let sum = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    return sum / arr.length;
+  ngOnChanges() {
+    console.log("I'm running!", this.cakeToShow);
+    const sum = this.cakeToShow.ratings.reduce((memo, currentRating) => {
+      console.log("We're finding", memo, currentRating);
+      return memo + currentRating.rating;
+    }, 0);
+    //
+    this.avg =
+      sum / this.cakeToShow.ratings.length || 'This cake is not yet rated';
+    console.log('our ACTUAL average is....', this.avg);
   }
+
+  // getAvg(arr) {
+  //   let sum = arr[0];
+  //   for (var i = 1; i < arr.length; i++) {
+  //     sum += arr[i];
+  //   }
+  //   return sum / arr.length;
+  // }
 
   ngOnInit() {}
 }
